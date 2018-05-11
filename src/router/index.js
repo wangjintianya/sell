7032d74cb532dbaf940e4b80cc6c421a27fc4126
wangjintianya <601221052@qq.com>
2018-05-11 09:15:54 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Goods from '@/components/Goods'
 
 Vue.use(Router)
 
@@ -8,8 +8,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Goods',
+      component: Goods
+    },
+    {
+      path: '/goods',
+      name: 'Goods',
+      component: Goods
     }
   ]
 })
